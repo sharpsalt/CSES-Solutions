2824d69c44201c4b4b9820a41b9a5f5f71c34446
Srijan Verma <bt23cse219@iiitn.ac.in>
2025-06-14 17:56:12 +0530

    Add files via upload

---
+++ Dynamic Programming/Grid_Paths_I.py
@@ -0,0 +1,19 @@
+MOD = 10**9 + 7
+n = int(input())
+grid = [input().strip() for _ in range(n)]
+dp = [[0] * n for _ in range(n)]
+if grid[0][0]=='.':
+    dp[0][0]=1
+
+for i in range(n):
+    for j in range(n):
+        if grid[i][j]=='*':
+            dp[i][j]=0
+        else:
+            if i>0:
+                dp[i][j]+=dp[i-1][j]
+            if j>0:
+                dp[i][j]+=dp[i][j-1]
+            dp[i][j]%=MOD
+
+print(dp[n-1][n-1])
